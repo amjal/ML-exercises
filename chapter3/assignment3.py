6f4766a0a37cc2a303b3f1377910ed92917b4c1d
amir jalili <jalilia370@gmail.com>
2020-10-11 18:17:41 +0330

    using pandas this time

--- chapter3/assignment3.py
+++ chapter3/assignment3.py
@@ -1,1 +1,13 @@
 import pandas
+
+# In this code I am going to assume that attributes are a mixture of continuous and discrete values
+training_set_address = input("Enter the path of training set .csv file: ")
+df = pandas.read_csv(training_set_address)
+print("training set successfully loaded")
+
+k = input("Enter k: ")
+
+# First, scale the values
+
+print(len(df))
+
