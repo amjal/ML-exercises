18dbf8d5a5a21b5248d6550aa967d05c4e6488c1
amir jalili <jalilia370@gmail.com>
2020-12-02 17:47:03 +0330

    linear classifer class added

--- chapter4/LinearClassifier.h
+++ chapter4/LinearClassifier.h
@@ -1,10 +1,19 @@
 #ifndef __LinearClassifier_h__
 #define __LinearClassifier_h__
-#include<vector>
+class example;
 class LinearClassifier{
 	private:
-		
+		// These are the labeled examples in the training set which we use to train the classifer
+		std::vector<example*> training_set;
+		// These are the new unlabeled examples that we give our classifier to label
+		std::vector<example*> dataset; 
+		// This is the polynomial order
+		uint8_t r;
+		// This is the number of attributes of each example
+		uint8_t n;
+		// These are the terms of the polynomial classifer
+		std::vector<int*
 	public:
-
+		inline LinearClassifer(std::vector<example*> training_set):training_set(training_set){}
 
 #endif
