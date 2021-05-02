7fbd2e66c9a29592bfd62b3db65804bdbf5935c2
amir jalili <jalilia370@gmail.com>
2021-05-02 22:44:43 +0430

    chapter2 test with UCI dataset

--- chapter2/assignment2.cpp
+++ chapter2/assignment2.cpp
@@ -1,16 +1,14 @@
-#include<iostream>
 #include<string>
 #include<map>
 #include<vector>
+#include"../cpp_csv_handler.h"
 
 int example_num = 0;
 
-struct example{
-	std::string class_label;
-	std::map<std::string, std::string> attribute_set;
-};
-
+// A map from class label to examples that are labeled with that class label
 std::map<std::string, std::vector<example>> example_set;
+// This has has the following structure: { class label --> {attribute value --> {probability of the attribute in the class}}}
+std::map<std::string, std::map<std::string, float>> attribute_probs;
 
 example classify(example ex){
 	float max_p = 0;
@@ -25,23 +23,26 @@ example classify(example ex){
 		pxci = 1;
 		//Iterate through all attributes of the ex example
 		for(std::map<std::string, std::string>::iterator att_it = ex.attribute_set.begin() ; att_it != ex.attribute_set.end(); att_it ++){
-			float att_num = 0;
-			float att_match = 0;
-			//Now iterate through all examples belonging to the same class calculating the probability P(Xi|Ci)
-			for(int i=0 ; i< (class_it->second).size() ; i++){
-				//First check if the example in vector has this particular attribute of ex
-				if((class_it->second)[i].attribute_set.count(att_it->first)){
-					att_num ++;
-					if((class_it->second)[i].attribute_set[att_it->first] == att_it->second){
-						att_match ++;
+			// Have we calculated the probability of this attribute for this class before?
+			if (attribute_probs[class_it -> first].count(att_it -> second) == 0){// If no then calculate it
+				float att_num = 0;
+				float att_match = 0;
+				//Now iterate through all examples belonging to the same class calculating the probability P(Xi|Ci)
+				for(int i=0 ; i< (class_it->second).size() ; i++){
+					//First check if the example in vector has this particular attribute of ex
+					if((class_it->second)[i].attribute_set.count(att_it->first)){
+						att_num ++;
+						if((class_it->second)[i].attribute_set[att_it->first] == att_it->second){
+							att_match ++;
+						}
 					}
 				}
+				attribute_probs[class_it ->first][att_it ->second] = att_match/att_num;
 			}
 			// This is naive bayesian assumption in which all attributes are assumed to be independent
-			pxci *= (att_match/att_num);
+			pxci *= attribute_probs[class_it ->first][att_it ->second];
 		}
 		pxci *= pci;
-		std::cout<<class_it->first<<": "<<pxci<<std::endl;
 		//Find which class maximaizes P(X|Ci)*P(Ci)
 		if(pxci > max_p){
 			max_p = pxci;
@@ -52,48 +53,20 @@ example classify(example ex){
 }
 		
 int main(){
-	printf("Please enter examples and their corresponding attributes in the following format:\n");
-	printf("<example class label>\\n<attr name><space><attr value>\\n[<attr name><space><attr value>\\n...]\\n\n");
-	printf("enter exit as example class label when finished\n\n");
-	while(true){
-		printf("Enter class label of the example:(enter done when you're done)\n");
-		// cl denotes class label of each example that the user enters
-		std::string cl;
-		std::getline(std::cin, cl);
-		if (cl == "exit") return 0;
-		if (cl == "done") break;
-		// number of examples will come handy in calculating P(Ci)
-		example_num ++;
-		//cl contains the class this example is labeled with
-		//Add the example to the corresponding class label. If class label doesn't exist add it.
-		if (example_set.find(cl) == example_set.end()){//This means the class doesn't yet exist in our example set
-			example_set[cl] = std::vector<example>();
-		}
-		example temp;
-		temp.class_label = cl;
-		printf("Now enter attribute values of this example:\n");
-		while(true){
-			std::string input;
-			std::getline(std::cin, input);
-			if(input.length() == 0) break;
-			int space_loc = input.find(' ');
-			std::string attr_name = input.substr(0, space_loc);
-			std::string attr_value = input.substr(space_loc+1 , input.length() - space_loc);
-			temp.attribute_set[attr_name] = attr_value;
-		}
-		example_set[cl].push_back(temp);
+	csvhandler file_handler;	
+	printf("Reading data from file...\n");
+	std::vector<example> example_vector = file_handler.get_examples("datasets/agaricus8.data");
+	example_num = example_vector.size();
+	printf("Grouping examples...\n");
+	example_set = file_handler.get_grouped_examples(example_vector);
+	printf("Running tests...\n");
+	int true_positive =0;
+	for(int i=0 ; i < example_vector.size(); i++){
+		std::string cl = example_vector[i].class_label;
+		example ex = classify(example_vector[i]);	
+		if(ex.class_label == cl)
+			true_positive ++;
 	}
-	printf("Now enter the attribute set for the example you want to label:\n");
-	example ex;
-	while(true){
-		std::string input;
-		std::getline(std::cin, input);
-		if(input.length() == 0) break;
-		int space_loc = input.find(' ');
-		std::string attr_name = input.substr(0, space_loc);
-		std::string attr_value = input.substr(space_loc +1, input.length() - space_loc);
-		ex.attribute_set[attr_name] = attr_value;
-	}
-	classify(ex);
+	printf("%f\n", float(true_positive)/float(example_vector.size()));
 	return 0;
 }
--- chapter2/readme.md
+++ chapter2/readme.md
@@ -2,3 +2,8 @@
 ## Computer Assignments
 
 ![Assignments](assignmentdefinitions/assignments.png)
+
+## Results
+### Discrete Bayesian Classification (assignment 2)
+	I used [this](https://archive.ics.uci.edu/ml/datasets/Mushroom) UCI dataset. I changed it a little bit and make eight different subsets of the whole dataset with sizes 1000, 2000, ..., 8120 examples. Then relabeled each subfile. Here are the results: 
+![graph](assignmentdefinitions/plot.png)
